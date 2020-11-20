f16bfeb2a740efb8644f04b8dbb37ef784256de8
Wolgrand Neto <wolgrand@live.com>
2020-11-20 18:32:48 -0300

    Add Floating Button

--- pages/home.tsx
+++ pages/home.tsx
@@ -15,6 +15,7 @@ export default function Home() {
   }
 
   return (
+    <>
     <nav className="flex items-center justify-between flex-nowrap bg-gray-800 p-6 fixed w-full z-10 top-0">
 
 
@@ -26,7 +27,7 @@ export default function Home() {
         </a>
       </div>
       <div className="flex justify-end items-center">
-          <button  id="nav-toggle" className="flex  items-center px-3 py-2  rounded text-gray-500  hover:bg-gray-900 hover:text-gray-300 hover:border-gray-300">
+          <button  id="nav-toggle" className="mr-3 hidden md:block sm:mr-0 flex items-center px-3 py-2  rounded text-gray-500  hover:bg-gray-900 hover:text-gray-300 hover:border-gray-300">
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
           </button>
           <p className="mr-5 ml-3 hidden text-lg text-gray-200 md:block">John Doe</p>
@@ -35,7 +36,13 @@ export default function Home() {
 
 	</nav>
 
+  <main>
+    <button  id="nav-toggle" className="fixed block sm:hidden items-center  bg-gray-800 bottom-6 right-6 w-16 h-16 rounded-full text-gray-500  hover:bg-gray-900 hover:text-gray-300 hover:border-gray-300">
+      <svg className="w-7 h-7 my-0 mx-auto" fill="none" stroke="#fff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
+    </button>
+  </main>
 
+    </>
   )
 }
 
