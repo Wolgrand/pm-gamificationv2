b12712f2beee586b84afc983aebd6b3bcf2af191
Wolgrand Neto <wolgrand@live.com>
2020-11-23 17:10:24 -0300

    Fix Overflow

--- pages/home.tsx
+++ pages/home.tsx
@@ -17,9 +17,9 @@ export default function Home() {
           <h1 className="text-gray-200 font-medium sm:text-4xl text-2xl">Ranking Geral</h1>
           <h3 className="text-yellow-500 mt-1 font-medium">Hoje | Dia 22 de novembro | domingo </h3>
         </div>
-        <div className="flex flex-col sm:pl-20 pt-4 px-5 mt-3 h-36">
+        <div className="flex flex-col sm:pl-20 pt-4 px-5 mt-3 h-32">
           <h2 className="text-gray-400 font-medium sm:text-2xl text-base border-solid border-b border-gray-600">Top 3 Departamentos</h2>
-          <div className="flex-row flex mt-3 h-full overflow-x-auto">
+          <div className="flex-row flex mt-3 overflow-x-auto h-32">
             <TopCard title="Departamento A" color="#f1c40f"/>
             <TopCard title="Departamento B" color="#95a5a6"/>
             <TopCard title="Departamento C" color="#e67e22"/>
