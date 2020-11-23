873a3c707bd5f9c21017c0ea1aec99bb005a01e3
Wolgrand Neto <wolgrand@live.com>
2020-11-23 18:39:02 -0300

    Fix Overflow

--- pages/home.tsx
+++ pages/home.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           <h1 className="text-gray-200 font-medium sm:text-4xl text-2xl">Ranking Geral</h1>
           <h3 className="text-yellow-500 mt-1 font-medium">Hoje | Dia 22 de novembro | domingo </h3>
         </div>
-        <div className="flex flex-col sm:pl-20 pt-4 px-5 mt-3 h-60">
+        <div className="flex flex-col sm:pl-20 pt-4 px-5 mt-3 h-64">
           <h2 className="text-gray-400 font-medium sm:text-2xl text-base border-solid border-b border-gray-600">Top 3 Departamentos</h2>
           <div className="flex-row flex mt-3 overflow-x-auto h-auto flex-nowrap min-h-0 ">
             <TopCard title="Departamento A" color="#f1c40f"/>
@@ -25,7 +25,7 @@ export default function Home() {
             <TopCard title="Departamento C" color="#e67e22"/>
           </div>
         </div>
-        <div className="flex flex-col sm:pl-20 pt-4 px-5 mt-3 ">
+        <div className="flex flex-col sm:pl-20 pt-4 px-5 mt-3 h-64">
           <h2 className="text-gray-400 font-medium sm:text-2xl text-base border-solid border-b border-gray-600">Top 3 Gerentes de Projetos</h2>
           <div className="flex-row flex mt-3 overflow-x-scroll">
             <TopCard title="Gerente A" color="#f1c40f"/>
