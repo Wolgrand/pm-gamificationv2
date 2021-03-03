69344a6f7413b0a53a8617b2ec69512c36378702
Wolgrand Neto <wolgrand@live.com>
2021-03-03 18:13:04 -0300

    Add informations page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -131,15 +131,15 @@ export default function Profile() {
           <div className="max-w-md mx-auto ">
             <div className="bg-gray-900 flex rounded-xl px-4 py-3 mb-2 justify-self-end align-middle items-center">
             <svg className="w-6 h-6 mr-3" fill="none" stroke="#6B7280" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" /></svg>
-              <Input readOnly name="name" className="bg-transparent text-white inline-block placeholder-white text-lg focus:bg-transparent w-full" type="text" placeholder="Nome" defaultValue={user.name ? user.name : ""}/>
+              <Input readOnly name="name" className="bg-transparent text-white inline-block placeholder-white text-lg focus:bg-transparent w-full" type="text" placeholder="Nome" value={user ? user.name : ""}/>
             </div>
             <div className="bg-gray-900 flex rounded-xl px-4 py-3 mb-2 justify-self-end align-middle items-center">
               <svg className="w-6 h-6 mr-3" fill="none" stroke="#6B7280" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg>
-              <Input readOnly name="email" className="bg-transparent text-white inline-block placeholder-white text-lg focus:bg-transparent w-full" type="text" placeholder="E-mail" defaultValue={user.email ? user.email : ""}/>
+              <Input readOnly name="email" className="bg-transparent text-white inline-block placeholder-white text-lg focus:bg-transparent w-full" type="text" placeholder="E-mail" value={user ? user.email : ""}/>
             </div>
             <div className="bg-gray-900 flex rounded-xl px-4 py-3 mb-6 justify-self-end align-middle items-center">
             <svg className="w-6 h-6 mr-3" fill="none" stroke="#6B7280" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" /></svg>
-              <Input readOnly name="department" className="bg-transparent text-white inline-block placeholder-white text-lg focus:bg-transparent w-full" type="text" placeholder="Department" defaultValue={user.department ? user.department : ""}/>
+              <Input readOnly name="department" className="bg-transparent text-white inline-block placeholder-white text-lg focus:bg-transparent w-full" type="text" placeholder="Department" value={user ? user.department : ""}/>
             </div>
             <div className="bg-gray-900 flex rounded-xl px-4 py-3 justify-self-end align-middle items-center mt-2 ">
               <svg className="w-6 h-6 mr-3" fill="none" stroke="#6B7280" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path></svg>
